c3e81bb3ec819d4c64daf9417cb061fd3ad5efeb
Valentin A. Popa <alv.popa@gmail.com>
2019-07-09 20:02:09 +0300

    basic routing added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useRoutes, A, navigate } from 'hookrouter';
+
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import AuthContextProvider from './contexts/AuthContext';
@@ -8,10 +10,37 @@ const client = new ApiService();
 
 const App = () => {
   const [isLoggedIn, setLoggedIn] = useState(client.isLoggedIn());
+
+  const routes = {
+    '/login': () => {
+      return isLoggedIn ? (
+        navigate('/dashboard')
+      ) : (
+        <Login setLoggedIn={setLoggedIn} />
+      );
+    },
+    '/signup': () => {
+      return isLoggedIn ? (
+        navigate('/dashboard')
+      ) : (
+        <Signup setLoggedIn={setLoggedIn} />
+      );
+    },
+    '/dashboard': () => {
+      return !isLoggedIn ? navigate('/') : <h1>Dashboard</h1>;
+    }
+  };
+
+  const routeResult = useRoutes(routes);
+
   return (
     <AuthContextProvider client={client}>
-      {!isLoggedIn && <Login setLoggedIn={setLoggedIn} />}
-      {!isLoggedIn && <Signup setLoggedIn={setLoggedIn} />}
+      {!isLoggedIn && <A href="/login">Login</A>}
+      <br />
+      {!isLoggedIn && <A href="/signup">Signup</A>}
+      <br />
+      {isLoggedIn && <A href="/dashboard">Dashboard</A>}
+      {routeResult}
     </AuthContextProvider>
   );
 };
